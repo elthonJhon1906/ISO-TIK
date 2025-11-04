f63a42210601804c05a56fe725a3dc54f748d2b5
Elthon Jhon Kevin <elthonjhonkevin@gmail.com>
2025-11-04 16:06:41 +0700

    setup plugin and library

---
+++ ISO-TIK/tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // agar Tailwind scan semua file React
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
